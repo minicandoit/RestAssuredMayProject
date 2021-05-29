835bc3bd85a3fbd8f1d26b2751afdfa9635684b2
minicandoit <74155239+minicandoit@users.noreply.github.com>
2021-05-29 13:04:45 -0400

    first commit

---
+++ src/test/java/com/Nestbridge/day1/Junit5Intro.java
@@ -0,0 +1,15 @@
+package com.Nestbridge.day1;
+
+import org.junit.jupiter.api.Test;
+
+public class Junit5Intro {
+
+   @Test
+   public void test(){
+      System.out.println("Junit5");
+   }
+
+
+
+
+}

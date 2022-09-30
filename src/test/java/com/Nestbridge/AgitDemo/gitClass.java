9fe7906de7cb642f8d941f33dbe23711f32e072f
minicandoit <minicandoit@hotmail.com>
2022-09-30 15:46:18 -0400

    Master added

--- src/test/java/com/Nestbridge/AgitDemo/gitClass.java
+++ src/test/java/com/Nestbridge/AgitDemo/gitClass.java
@@ -4,6 +4,7 @@ public class gitClass {
     public static void main(String[] args) {
         int a= 1;
                 String b="Master";
+                //
         System.out.println(b);
         System.out.println(a);
         System.out.println("to merge");
